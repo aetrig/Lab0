3696f5ded5e6512f8747439208b5d2927704e45d
aetrig <emilia.gosztyla@gmail.com>
2025-03-06 16:06:34 +0100

    Add project files.

---
+++ Lab0/FizzBuzz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab0
+{
+    class FizzBuzz
+    {
+        private int max;
+        public FizzBuzz(int max)
+        {
+            this.max = max;
+        }
+        public void Print()
+        {
+            for (int i = 1; i <= max; i++)
+            {
+                String outputString = "";
+                if (i % 3 == 0) outputString += "Fizz";
+                if (i % 5 == 0) outputString += "Buzz";
+                if (outputString == "") Console.WriteLine(i);
+                else Console.WriteLine(outputString);
+            }
+        }
+    }
+}
---
+++ Lab0/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Lab0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz FizzBuzz1 = new(20);
+            FizzBuzz1.Print();
+        }
+    }
+}
